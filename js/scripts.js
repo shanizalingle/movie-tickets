570b2fbc206066446f34b8d4b5eb142a437cf8b6
Shaniza Lingle <shanizalingle@gmail.com>
2022-08-23 16:28:05 -0700

    add UI Logic in JS to calculate and display price
    Co-authored-by: Harold Mesa <harold.mesa@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,32 @@ Input.prototype.calculate = function() {
 }
 
 // User Interface Logic
-/* let ticket = new Ticket();
-let input = new Input(); */
-
-if (input.calculate() > 6 ) {
- console.log("It works!");
+function handleSubmission(event) {
+  event.preventDefault()
+  const inputtedMovie = parseInt(document.getElementById("movie-input").value);
+  const inputtedTimeOfDay = parseInt(document.querySelector("input[name='timeOfTheDay']:checked").value);
+  const inputtedAgeGroup = parseInt(document.getElementById("userAge-input").value);
+  let input = new Input(inputtedMovie, inputtedTimeOfDay, inputtedAgeGroup);
+  
+  let price;
+  if (input.calculate() > 6 ) {
+    price = 15;
+  } else if (input.calculate() < 6 ) {
+    price = 12;
+  } else {
+    price = 8;
+  }
+  
+  document.getElementById("results").innerHTML = price;
+  document.getElementById("resultsDiv").removeAttribute("class");
 }
 
+function resetForm() {
+  document.getElementById("inputForm").reset();
+  }
+
+window.addEventListener ("load", function() {
+  let form = document.getElementById("inputForm");
+  form.addEventListener("submit", handleSubmission);
+  form.addEventListener("submit", resetForm);
+});
